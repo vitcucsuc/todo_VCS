f6b56f7791fdf539cbb2140e503617125f831085
vitcucsuc <tuankim12341475@gmail.com>
2026-01-05 00:16:01 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,31 @@ import taskRoute from "./routes/taskRouters.js";
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+if (process.env.NODE_ENV != "production") {
+    app.use(cors());
+}
 
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const BASE_URL =
+    import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api"
+
 const api = axios.create({
     baseURL: "http://localhost:3001/api"
 })
