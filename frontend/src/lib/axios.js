bb87db556ea746aa2d6088d443d6cb0c62259aee
vitcucsuc <tuankim12341475@gmail.com>
2026-01-05 00:32:58 +0700

    deploy lan 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const BASE_URL =
-    import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development"
+    ? "http://localhost:3001/api"
+    : "/api";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001/api"
-})
+    baseURL: BASE_URL,
+    timeout: 5000,
+});
 
 export default api;
